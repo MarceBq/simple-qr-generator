cd5e1ea4ea4df27e9269ea3143c0a4322f753549
Marcelo <marcebq2005@gmail.com>
2024-03-02 04:05:55 -1200

    primer commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const buttonElement = document.querySelector('.button');
+const inputElement = document.querySelector('.input-text');
+
+const openQrPage = () => {
+    window.location.href = './qr.html';
+}
+
+buttonElement.addEventListener('click', () => {
+    if (inputElement.value  === '' || inputElement.value  === ' ') {
+        alert('Please enter a valid URL');
+    } else {
+        localStorage.setItem('url', inputElement.value);    
+        openQrPage();
+    }
+});
---
+++ qr.js
@@ -0,0 +1,47 @@
+const qrCodeContainer = document.querySelector('.qr-code');
+const downloadButton = document.querySelector('.dowload-button');
+const shareButton = document.querySelector('.share-button');
+
+// Get url from local storage
+const url = localStorage.getItem('url');
+
+
+
+// Create qr code
+const qrCode = new QRCode(qrCodeContainer, {
+    width: 200,
+    height: 200,
+    colorDark: '#000000',
+    colorLight: '#ffffff',
+});
+
+// Create qr code
+function createQrCode(){
+    qrCode.makeCode('url');
+}
+
+createQrCode();
+
+function donwloadImg(){
+    const qrCodeImage = document.querySelector('.qr-code img');
+
+    const img = qrCodeImage.src;
+    const linkImg = document.createElement('a');
+    linkImg.href = img;
+    linkImg.download = 'qr-code.png';
+    linkImg.click();
+    linkImg.remove();
+}
+
+downloadButton.addEventListener('click', () =>{
+    donwloadImg();
+});
+
+// Share qr code 
+shareButton.addEventListener('click', () => {  
+    navigator.share({
+        title: 'QR Code',
+        text: 'QR Code',
+        url: url,
+    })
+})
